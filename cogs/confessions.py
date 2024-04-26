61cb56b04dba88ff447062145155c194e4768c57
Shinro Chisadin <92295497+NamfromVN@users.noreply.github.com>
2024-04-27 00:21:00 +0700

    Modify code structure
    Check before merge!

--- cogs/confessions.py
+++ cogs/confessions.py
@@ -56,31 +56,32 @@ class Confession(commands.Cog):
 
     @commands.command(name="cfsan", description=f"- Tạo confession ẩn danh\n- **Cách sử dụng**: nhập lệnh `{command_prefix}cfsan + nội dung`")
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def create_anonymous_confession(self, ctx, *, content=""):
-        if ctx.channel.id == self.confession_channel_id: 
-            if not content: 
-                await ctx.message.delete()
-                embed = disnake.Embed(description=f"**Cách sử dụng**: nhập lệnh `{self.command_prefix}cfs + nội dung`", color=0xfd5296)
-                temp = await ctx.send(embed=embed)
-                await asyncio.sleep(5)
-                await temp.delete()
-            else:
-                self.cfs_count += 1
-                await ctx.message.delete()
-                embed = disnake.Embed(title=f"CFS #{self.cfs_count}", description=content, timestamp=datetime.datetime.now(datetime.UTC), color=0xfd5296) 
-                embed.set_footer(text=f"Confession ẩn danh")
-                confession_channel = self.bot.get_channel(self.confession_channel_id)
-                message = await confession_channel.send(embed=embed)
-                thread = await message.create_thread(name=f"Trả lời CFS #{self.cfs_count}")
-                self.cfs_data["count"] = self.cfs_count
-                self.save_cfs_data()
-        else: 
+    async def create_anonymous_confession(self, ctx, *, content = None):
+        if ctx.channel.id != self.confession_channel_id: 
             await ctx.message.delete()
             crt_channel = f"https://discord.com/channels/{ctx.guild.id}/{self.confession_create_channel_id}"
             temp = await ctx.send(embed = disnake.Embed(description=f"**Bạn chỉ có thể tạo confessions ở kênh {crt_channel} .**",color=0xff0000))
             await asyncio.sleep(5)
             await temp.delete()
             return
+            
+        if content is None: 
+                await ctx.message.delete()
+                embed = disnake.Embed(description=f"**Cách sử dụng**: nhập lệnh `{self.command_prefix}cfs + nội dung`", color=0xfd5296)
+                temp = await ctx.send(embed=embed)
+                await asyncio.sleep(5)
+                await temp.delete()
+                return 
+        
+        self.cfs_count += 1
+        await ctx.message.delete()
+        embed = disnake.Embed(title=f"CFS #{self.cfs_count}", description=content, timestamp=datetime.datetime.now(datetime.UTC), color=0xfd5296) 
+        embed.set_footer(text=f"Confession ẩn danh")
+        confession_channel = self.bot.get_channel(self.confession_channel_id)
+        message = await confession_channel.send(embed=embed)
+        thread = await message.create_thread(name=f"Trả lời CFS #{self.cfs_count}")
+        self.cfs_data["count"] = self.cfs_count
+        self.save_cfs_data()
 
     def save_cfs_data(self):
         with open(self.cfs_file, "w") as f:
